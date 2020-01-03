0819c72b4e57ff54917a500bbaf030b5c04b040b
boyphongsakorn <boy1556@hotmail.com>
2020-01-03 14:48:49 +0000

    Deleted file/Apache.pdf, file/Array.pdf, file/CURL.pdf, file/Date.pdf, file/File_Directory.pdf, file/General.pdf, file/HJM.pdf, file/Network.pdf, file/PDO.pdf, file/RS.pdf, file/mysqli.pdf, file/phpinstrpdffile.pdf, file/zip.pdf, image/php.gif, image/phpintro.jpg, admin.php, checklogin.php, checkregister.php, exe.php, index.php, instrmedia.php, learnphpme_phpdata.sql, login.php, logout.php, settings.php, showmyexe.php, submitexe.php files

--- settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost','username','password','database');
-mysqli_set_charset($mysqli, "utf8");
-$selectdata = "SELECT * FROM header";
-$querydata = $mysqli->query($selectdata);
-?>
